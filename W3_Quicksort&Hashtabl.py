51c843cf15ea2291e4fff7054f63fb60e49c783d
jate-beatles <jate@live.cn>
2023-03-16 22:40:41 +0100

    just it

---
+++ W3_Quicksort&Hashtabl.py
@@ -0,0 +1,22 @@
+# Algorithms that use randomness 
+def geometric(p):
+    count = 0 
+    if flip(p):
+        return 1 + geometirc(p)
+    else: return 1 
+# the worst case is the always flipping the coin 
+# why randomness: 
+# Aveage Case Complexity: expected case 
+# 
+##Why randomness in Data-structure - Hashtables / Treaps 
+#in many case store not random data, introduce the randomness into data constructure.
+#
+## Analysis of Algorithms: Recurrences 
+#           
+####Partition and Quicksort 
+## Quick Sort - practical methods 
+#
+#
+#
+#
+#
