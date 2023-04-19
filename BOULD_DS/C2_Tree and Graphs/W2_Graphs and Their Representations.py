62797a175acce7ccc732fb2f3a08fa2026fb7313
jate-beatles <jate@live.cn>
2023-04-19 23:58:35 +0200

    gogogo

--- BOULD_DS/C2_Tree and Graphs/W2_Graphs and Their Representations.py
+++ BOULD_DS/C2_Tree and Graphs/W2_Graphs and Their Representations.py
@@ -1,8 +1,11 @@
 #### 
 ####        Panda Asian
 #               
-#
-#
+#           -what is graphs? relation and the initia net;
+#           - V = {v1, v2, v3, v4}
+#              find the different combination 
+#              direct & undirect graph
+#                   undirect has two way direct
 #
 #
 #
