8d4569574255e4aeb7ca03e681c24cab39d197d1
jate-beatles <jate@live.cn>
2024-01-26 23:40:26 +0100

    |r||s|cos

--- Mathematics_for_ML.py/C1_W1W2.py
+++ Mathematics_for_ML.py/C1_W1W2.py
@@ -20,13 +20,19 @@
 ##### Distributive over addition: r .(s+t) = r.s + r.t
 ##### Associatie over scalar multiplication: r.(as) = a(r.s)  ##a is the multiple scalar number
 ##### if vector length is the multiply its self
+################    r.r = r1.r1 + r2.r2 = r1^2 + r2^2 = |r|^2
 
 #############################################################################
 #### cosince rules  c^2 = a^2 + b^2 - 2abcos(theta)
 #                   |r-s|^2 = |r|^2 + |s|^2 - 2|r||s|cos(theta)
 # we can use the vector to figure out by using cot product vecto it self then ^1/2 to get the size of the vectors
+#                   |r-s|^2 = (r-s).(r-s) = r.r -s.r - s.r + s.s
+#         |r|^2 - 2r.s + |s|^2 =  |r|^2 + |s|^2 - 2|r||s| cos(theta)
+#       r.s = |r| . |s| . cos(theta)
+#       if the cosine is 90 degree, then cos(90) is 0, the r.s is the 0
+#       if the cosine is 180 degree, then cos(180) is -1, the r.s will be -|s||r| 
 #
-#
+#     
 
 
 
